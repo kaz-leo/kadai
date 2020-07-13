ddb260b728921d2d724e850e775c54081e078e59
Leo <kaz84program@gmail.com>
2020-07-13 15:41:32 +0000

    課題再

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
-  validates :name, presence: true ,length:{maximum:15}
+  
   VALID_EMAIL_REGEX = /\A[a-z\d]+@[a-z\d]+.[a-z\d]+\z/
-  validates :email, presence: true , format:{wuth: VAlID_EMAIL_REGEX}
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\z/
+ 
+  validates :name, presence: true ,length:{maximum:15}
+  validates :email, presence: true , format:{wuth: VAlID_EMAIL_REGEX}
   validates :password, length:{minimum:8 , maximum:32}, format:{with: VALID_PASSWORD_REGEX}
   validates :password_confirmation, length: {minimum:8 , maximum:32}, format:{with: VALID_PASSWORD_REGEX}
   
